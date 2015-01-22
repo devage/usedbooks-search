3ed0e15b1b0ce144fca533bb8895a321c7aff536
ikpark <ikpark@gmail.com>
2015-01-22 09:31:23 +0900

    edit desc and url.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject usedbooks-search "0.1.0-SNAPSHOT"
-  :description "FIXME: write this!"
-  :url "http://example.com/FIXME"
+  :description "Search for used books on the offline stores of aladin.co.kr."
+  :url "https://github.com/vvalkyrie/usedbooks-search"
 
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/clojurescript "0.0-2657"]]
