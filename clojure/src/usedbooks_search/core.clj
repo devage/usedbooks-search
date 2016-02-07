ca66d1f1f8c9167040710e019dc1256fff6811e8
ikpark <ikpark@gmail.com>
2016-02-07 20:45:37 +0900

    port aladin query

--- clojure/src/usedbooks_search/core.clj
+++ clojure/src/usedbooks_search/core.clj
@@ -2,10 +2,62 @@
   (:require [net.cgrand.enlive-html :as html])
   (:gen-class))
 
+(def base-url "http://off.aladin.co.kr/usedstore/wsearchresult.aspx")
+
+(def offline-shops
+  ["gangnam"
+   "geondae"
+   "gwangju"
+   "nowon"
+   "daegu"
+   "daejeon"
+   "daehakro"
+   "busan"
+   "bucheon"
+   "bundang"
+   "sanbon"
+   "suwon"
+   "sillim"
+   "sinchon"
+   "yeonsinnae"
+   "ulsan"
+   "ilsan"
+   "sincheon"
+   "jeonju"
+   "jongno"
+   "cheonan"
+   "cheongju"])
+
+(defn usedstore-url [shop encoded-query]
+  (str base-url "?offcode=" shop "&SearchWord=" encoded-query))
+
+(defn str->euckr-seq [^String s]
+  (seq (.getBytes s "euc-kr")))
+
+(defn encode-aladin-percent [s]
+  (map (fn [c]
+         (cond
+           (= c 0x20) "+"      ; ' '
+           (= c 0x5c) ""       ; '\'
+           (or (< 0x2f c 0x4a) ; 0-9
+               (< 0x40 c 0x5b) ; A-Z
+               (< 0x60 c 0x7b) ; a-z
+               (= c 0x2a)      ; '*'
+               (= c 0x2d)      ; '-'
+               (= c 0x2e)      ; '.'
+               (= c 0x5f)) (char c) ; '_'
+           :else (str "%" (format "%02X" (if (< c 0) (+ c 256) c))))) s))
+
+(defn encode-aladin-query [qry]
+  (clojure.string/join
+    (encode-aladin-percent (str->euckr-seq qry))))
+
 (defn fetch-url [url]
   (html/html-resource (java.net.URL. url)))
 
 (defn -main [& args]
-  (if (> (count args) 0)
-    (println "Hello," (first args))
-    (println "Hello, World")))
+  (if (= (count args) 0)
+    (println "USAGE: usedbooks-search <query-string>")
+    (println
+      (fetch-url (usedstore-url "jongno"
+                                (encode-aladin-query (first args)))))))
